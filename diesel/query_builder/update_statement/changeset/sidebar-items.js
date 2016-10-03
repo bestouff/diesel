051ba21bf93f457694040d1f62a8215e6d137f7a
Travis CI User <travis@example.org>
2016-10-03 22:18:56 +0000

    Documentation for diesel-rs/diesel@60ae167

---
+++ diesel/query_builder/update_statement/changeset/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"trait":[["AsChangeset","Types which can be passed to `update.set`. This can be automatically generated for structs by `#[derive(AsChangeset)]`."],["Changeset","Apps should not need to concern themselves with this trait."]]});
